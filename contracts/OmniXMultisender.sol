2c46c9a57c46a9c07799d073960a94ea5da4bd53
exakoss <daniel@omni-x.io>
2024-06-03 16:19:51 -0400

    change: remove unchecked from estimate fees in the multisender

--- contracts/OmniXMultisender.sol
+++ contracts/OmniXMultisender.sol
@@ -227,16 +227,14 @@ contract OmniXMultisender is Initializable, Clone {
         bytes[] calldata messages,
         bytes[] calldata options
     ) external view virtual returns (uint256[] memory nativeFees) {
-        unchecked {
-            nativeFees = new uint256[](dstEids.length);
-            for (uint256 i; i < dstEids.length; ++i) {
-                nativeFees[i] = endpoint().quote(
-                    MessagingParams(
-                        dstEids[i], _getPeer(dstEids[i]), messages[i], options[i], false
-                    ),
-                    address(this)
-                ).nativeFee;
-            }
+        nativeFees = new uint256[](dstEids.length);
+        for (uint256 i; i < dstEids.length; ++i) {
+            nativeFees[i] = endpoint().quote(
+                MessagingParams(
+                    dstEids[i], _getPeer(dstEids[i]), messages[i], options[i], false
+                ),
+                address(this)
+            ).nativeFee;
         }
     }
 
